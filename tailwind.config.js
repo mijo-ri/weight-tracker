884aaca1ad9c153a1077fb358c5986174d843664
Michael John Rieser <michi.rieser@gmail.com>
2021-12-23 14:37:48 +0100

    Add tailwindcss.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: '"Open Sans", Helvetica, Arial, sans-serif',
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
